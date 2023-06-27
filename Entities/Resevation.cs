8a47f3db9f87f478c2a0469d8c1e5ec19b0d92b4
Douglas Nunes <paulodnunes2016@gmail.com>
2023-06-27 00:10:35 -0300

    Entrada e saida

---
+++ Entities/Resevation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Entities
+{
+    internal class Resevation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime  Chekin { get; set; }
+        public DateTime Chekout { get; set; }             
+
+        public Resevation()
+        {
+        }
+        public Resevation(int roomNumber, DateTime chekin, DateTime chekout)
+        {
+            RoomNumber = roomNumber;
+            Chekin = chekin;
+            Chekout = chekout;
+        }
+        public int Durantion()
+        {
+            TimeSpan durantion = Chekout.Subtract(Chekin);
+            return (int)durantion.TotalDays;
+        }
+
+        public void UpdateDates(DateTime chekin,DateTime chekout)
+        {
+            Chekin = chekin;
+            Chekout = chekout;
+        }
+
+        public override string ToString()
+        {
+            return " Room "
+                + RoomNumber
+                + " , Checkin: "
+                + Chekin.ToString(" dd/mm/yyyy ")
+                + " , Check - Out: "
+                + Chekin.ToString(" dd/mm/yyyy ")
+                + " , "
+                + Durantion()
+                + " Nights ";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Hotel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write(" Chek-in- data (dd/mm/yyyy) ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write(" Chek-out data (dd/mm/yyyy) ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if (checkout <= checkin)
+            {
+                Console.WriteLine(" Erro in resevation: Chek Out must date be after chekin date");
+            }
+            else
+            {
+                Resevation resevation = new Resevation(number, checkin, checkout);
+                Console.WriteLine(" Resevation: " + resevation);
+
+                Console.WriteLine();
+                Console.WriteLine(" Enter date to update the resevation: ");
+                Console.Write(" Chek-in- data (dd/mm/yyyy) ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write(" Chek-out data (dd/mm/yyyy) ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkin < now || checkout < now)
+                {
+                    Console.WriteLine(" Erro in resevation: resevation dates for update must be future dates");
+                }
+                else if (checkout <= checkin)
+                {
+                    Console.WriteLine(" Erro in resevation: Chek Out must date be after chekin date");
+                }
+                else
+                {
+                    resevation.UpdateDates(checkin, checkout);
+                    Console.WriteLine(" resvation: " + resevation);
+                }
+            }
+
+        }
+
+    }
+}
